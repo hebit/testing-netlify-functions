00f6a0a38fc012cfb01add0ef80fd766a888ef1b
Hebert Oliveira <hebertoliveira@infojr.com.br>
2020-10-13 22:25:22 -0300

    create first function

---
+++ .netlify/functions/hello.js
@@ -0,0 +1,13 @@
+import fetch from "node-fetch";
+
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com/todos/1";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
+};
